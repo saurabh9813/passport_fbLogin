8bf188ae65e44b73c41091ffbecf981d0b0ac08a
saurabh9813 <saurabh_2002@hotmail.com>
2016-01-21 17:42:00 +0530

    fb Login app udloaded

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '547793675373133',
+		'clientSecret': 'b55f3f4b7e73bc916032fd146e4f8974',
+		'callbackURL': 'http://localhost:3040/auth/facebook/callback'
+	},
+}
